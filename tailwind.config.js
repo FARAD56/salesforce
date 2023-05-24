ee2b1c3a1ba8042c48d1259c56cf2ceb36e0758e
Abubakari Al-Waasiu <alwaasiua@gmail.com>
2023-05-24 22:09:57 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'yel-green': '#BDE038',
+        'greenish-slate':'#A3AB78',
+        'ash': '#10454F',
+        
+      },
+    },
+
+  },
+  plugins: [],
+ 
+}
